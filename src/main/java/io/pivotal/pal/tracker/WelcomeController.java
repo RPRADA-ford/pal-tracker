dae03fbdc66f064d470138e1d124f3fd3a2d4c16
RPRADA-ford <rprada@ford.com>
2020-09-22 08:38:46 +0000

    LAB completed

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,21 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    private String message;
+
+    @Value("${welcome.message}")
+    private void message(String messageParam) {
+        message = messageParam;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}
